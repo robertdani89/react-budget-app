d3f66094eb8f482233e28a893eab7036feb7c28b
robert-dani <robert.dani@onrobot.com>
2022-01-17 12:43:35 +0100

    added Budget Context

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react"
 import ReactDom from "react-dom"
 import App from "./App"
 import "bootstrap/dist/css/bootstrap.min.css"
+import { BudgetsProvider } from "./contexts/BudgetsContext"
 
 ReactDom.render(
     <React.StrictMode>
-        <App />
+        <BudgetsProvider>
+            <App />
+        </BudgetsProvider>
     </React.StrictMode>,
     document.getElementById("root")
 )
