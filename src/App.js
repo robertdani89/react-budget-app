cccbca4c11a0f4d648dc33a02d56bb538715dfa8
robert-dani <robert.dani@onrobot.com>
2022-01-17 10:10:02 +0100

    header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Button, Container, Stack } from "react-bootstrap"
+
 function App() {
-    return "hello world"
+    return (
+        <Container className="my-4">
+            <Stack direction="horizontal" gap="2" className="mb-4">
+                <h1 className="me-auto">Budgets</h1>
+                <Button variant="primary">Add budget</Button>
+                <Button variant="online-primary">Add expense</Button>
+            </Stack>
+        </Container>
+    )
 }
 
 export default App
